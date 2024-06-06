25e5be026ac5ce4e1e8ca2e87147c3cbd879b97f
Ana Karoline <anadevkarol@gmail.com>
2024-06-06 18:23:57 -0300

    update configurações

--- src/pages/Configuracao/index.js
+++ src/pages/Configuracao/index.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StatusBar } from "expo-status-bar";
 import Slider from '@react-native-community/slider';
-import { View, Text, StyleSheet, Image, ScrollView, Linking, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, Image, ScrollView, Linking, TouchableOpacity, Modal } from 'react-native';
 
-export default function Config({ navigation }) {
+export default function Config({ navigation, userName }) {
+  const [showTermos, setShowTermos] = useState(false);
+  const [showSobreNos, setShowSobreNos] = useState(false);
+  const [showAjuda, setShowAjuda] = useState(false); 
+
   const handlePress = () => {
     Linking.openURL('https://www.youtube.com/watch?v=jsn9leBhKik');
   };
@@ -17,22 +21,21 @@ export default function Config({ navigation }) {
       <ScrollView contentContainerStyle={styles.scrollContainer}>
         <View style={styles.card}>
           <Image style={styles.img} source={require('../../assets/logoong.png')} />
-          <Text style={styles.titleText}>ONG BANCO DE ALIMENTOS</Text>
         </View>
         <View>
           <Text style={styles.config}>CONFIGURAÇÕES</Text>
         </View>
         <View style={styles.newSquare}>
           {/* Quadrados pequenos */}
-          <TouchableOpacity style={styles.smallSquare}>
+          <TouchableOpacity style={styles.smallSquare} onPress={() => setShowTermos(true)}>
             <Text style={styles.squareText}>Termos de Serviço</Text>
             <Text style={styles.arrow}>→</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.smallSquare}>
+          <TouchableOpacity style={styles.smallSquare} onPress={() => setShowSobreNos(true)}>
             <Text style={styles.squareText}>Sobre Nós</Text>
             <Text style={styles.arrow}>→</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.smallSquare}>
+          <TouchableOpacity style={styles.smallSquare} onPress={() => setShowAjuda(true)}> 
             <Text style={styles.squareText}>Ajuda/FAQ</Text>
             <Text style={styles.arrow}>→</Text>
           </TouchableOpacity>
@@ -57,6 +60,82 @@ export default function Config({ navigation }) {
         <Image style={styles.imgDade} source={require('../../assets/acessibilidade.png')} />
       </ScrollView>
       <StatusBar style="auto" />
+
+      {/* Modal para exibir os Termos de Serviço */}
+      <Modal visible={showTermos} animationType="slide" transparent>
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text style={styles.modalTitle}>Bem-vindo aos nossos Termos de Serviço!</Text>
+            <Text style={styles.modalText}>
+            Ao fazer parte desta comunidade, você concorda em respeitar as opiniões,
+            ideias e direitos dos outros membros. Estamos comprometidos em promover 
+            a diversidade, a inclusão e o respeito mútuo em todos os aspectos da nossa plataforma.
+            Ao utilizar nossos serviços, você concorda em cumprir nossos termos
+            de uso e diretrizes da comunidade. Isso inclui, mas não se limita a,
+            manter a civilidade em suas interações, respeitar a privacidade dos 
+            outros usuários e não violar os direitos autorais ou outros direitos de propriedade intelectual.
+            </Text>
+            <TouchableOpacity onPress={() => setShowTermos(false)} style={styles.closeButton}>
+              <Text style={styles.closeButtonText}>Fechar</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
+      {/* Modal para exibir a seção de Ajuda/FAQ */}
+      <Modal visible={showAjuda} animationType="slide" transparent>
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text style={styles.modalTitle}>Ajuda/FAQ</Text>
+            <Text style={styles.modalText}>
+            <Text style={styles.boldText}>1. Como faço para sair da conta?</Text>
+              {'\n\n'}
+              Vá para a página do perfil e clique em 'Sair'
+              {'\n\n'}
+              <Text style={styles.boldText}>2. Quais são os recursos disponíveis na plataforma?</Text>
+              {'\n\n'}
+              Você pode visualizar ONG's disponiveis e seus pontos de coleta e também cadastrar informações caso seja uma ONG.
+              {'\n\n'}
+              <Text style={styles.boldText}>3. Como acesso meu perfil?</Text>
+              {'\n\n'}
+              Clique no icon de usuário na parte inferior da tela
+              {'\n\n'}
+              <Text style={styles.boldText}>4. O que faço se esqueci minha senha?</Text>
+              {'\n\n'}
+              Entre em contanto com o suporte
+              {'\n\n'}
+              <Text style={styles.boldText}>5. Como faço para entrar em contato com o suporte técnico?</Text>
+              {'\n\n'}
+              Na tela de "Configurações" clique em "Fale conosco". 
+              Você será encaminhado para o chat com o suporte.
+            </Text>
+            <TouchableOpacity onPress={() => setShowAjuda(false)} style={styles.closeButton}>
+              <Text style={styles.closeButtonText}>Fechar</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
+
+      {/* Modal para exibir informações sobre a empresa */}
+      <Modal visible={showSobreNos} animationType="slide" transparent>
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text style={styles.modalTitle}>Sobre Nós</Text>
+            <Text style={styles.modalText}>
+            Somos uma equipe de estudantes de ciência da computação unidos
+            por nossa paixão pela tecnologia e inovação. 
+            Nosso objetivo é criar uma comunidade onde pessoas de todas
+             as origens possam se conectar, aprender e crescer juntas. 
+             Estamos comprometidos em transformar ideias em realidade, 
+             impulsionados pela curiosidade, criatividade e desejo de
+             fazer a diferença. Junte-se a nós nesta jornada emocionante
+             e seja parte da nossa história!
+            </Text>
+            <TouchableOpacity onPress={() => setShowSobreNos(false)} style={styles.closeButton}>
+              <Text style={styles.closeButtonText}>Fechar</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
@@ -107,7 +186,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#712685',
     width: 330,
     height: 40,
-    marginTop: 7,
+    marginTop: 10,
     borderRadius: 20,
     flexDirection: 'row',
     justifyContent: 'space-between',
@@ -143,6 +222,9 @@ const styles = StyleSheet.create({
     margin: 12,
     marginTop: -10,
   },
+  boldText: {
+    fontWeight: 'bold',
+  },
   bottomSquareContainer: {
     alignItems: 'center'
   },
@@ -168,5 +250,40 @@ const styles = StyleSheet.create({
     alignSelf: 'flex-end',
     marginTop: 52,
     marginRight: 20,
+  },
+  modalContainer: {
+    position: 'absolute',
+    top: '-10%',
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalContent: {
+    backgroundColor: '#fff',
+    padding: 20,
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  modalTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  modalText: {
+    fontSize: 16,
+    textAlign: 'justify',
+    marginBottom: 20,
+  },
+  closeButton: {
+    backgroundColor: '#8D579A',
+    padding: 10,
+    borderRadius: 5,
+    alignSelf: 'flex-end',
+  },
+  closeButtonText: {
+    color: '#fff',
+    fontWeight: 'bold',
   },
 });
